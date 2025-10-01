26019bbb070c274833de91f778fb1e35c0f587de
Larissa Laumann <larissalaumann1@gmail.com>
2025-09-30 21:08:23 -0300

    Add files via upload

---
+++ db_recuperacao.sql
@@ -0,0 +1,22 @@
+
+create database dbb_recuperacao;
+use dbb_recuperacao;
+    
+create table tb_usuario (
+	id int not null auto_increment primary key,
+    nome varchar(100),
+    email varchar(50),
+    senha varchar(15)
+    );
+    
+
+create table tb_livros (
+    id int not null auto_increment primary key,
+    nomeLivro varchar(100),
+    editora varchar(100),
+    ano date,
+    disponivel boolean default false,
+    usuario_id int,
+    foreign key (usuario_id) references tb_usuario(id)
+);
+
